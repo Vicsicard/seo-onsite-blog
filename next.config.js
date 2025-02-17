651949f4537695c1a6f4f28ac39c7e06312beba2
Vicsicard <vicsicard@gmail.com>
2025-02-17 16:22:49 -0700

    Convert Next.js config to JavaScript and add image configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
